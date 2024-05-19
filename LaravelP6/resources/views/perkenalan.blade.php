c3cf4c06d280dd9480c1b3b03821d9b377afbc29
aryobudipratama <aryobudipratama07@gmail.com>
2024-05-19 13:05:04 +0700

    Pertemuan6

---
+++ LaravelP6/resources/views/perkenalan.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perkenalan Diri</title>
+    <style>
+        body {
+            background: black;
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+        }
+
+        h1 {
+            color: #ffffff;
+            text-align: center;
+            padding-top: 30px;
+        }
+
+        h2 {
+            color: #ffffff;
+            margin-top: 30px;
+        }
+
+        p {
+            color: #ffffff;
+            line-height: 1.5;
+            margin-left: 20px;
+        }
+
+        ul {
+            list-style-type: disc;
+            margin-left: 20px;
+            color: #ffffff;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 60px;
+            
+        }
+
+        /* Warna-warna tambahan */
+        .bg-1 {
+            background-color:  rgb(0, 0, 0);
+            border: 3px solid  aqua;
+            color: #ffffff;
+            padding: 10px;
+            border-radius: 20px;
+            margin-bottom: 20px;
+        }
+
+        .bg-2 {
+            background-color:  rgb(0, 0, 0);
+            border: 3px solid  rgb(255, 0, 255);
+            color: #ffffff;
+            padding: 10px;
+            border-radius: 20px;
+            margin-bottom: 20px;
+        }
+
+        .bg-3 {
+            background-color:  rgb(0, 0, 0);
+            border: 3px solid  red;
+            color: #ffffff;
+            padding: 10px;
+            border-radius: 20px;
+            margin-bottom: 20px;
+        }
+
+        .bg-4 {
+            background-color:  rgb(0, 0, 0);
+            border: 3px solid  greenyellow;
+            color: #ffffff;
+            padding: 10px;
+            border-radius: 20px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    
+    <div class="container">
+        <h1 class="bg-1">Biodata Saya</h1>
+
+        <h2 class="bg-2">Informasi Pribadi</h2>
+        <p>Nama: Aryo Budi Pratama</p>
+        <p>Alamat: Jl. Walang Permai Koja Jakarta Utara</p>
+
+
+        <h2 class="bg-3">Pendidikan</h2>
+        <ul>
+            <li>Universitas Indraprasta PGRI</li>
+        </ul>
+
+        <h2 class="bg-4">Minat dan Hobi</h2>
+        <ul>
+            <li>Bermain basket</li>
+            <li>Bermain game</li>
+            <li>Traveling</li>
+        </ul>
+    </div>
+</body>
+
+</html>
---
+++ LaravelP6/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/halo', function () {
+    return view('halo');
+});
+Route::get('/perkenalan', function () {
+    return view('perkenalan');
+});
